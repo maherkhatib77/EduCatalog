c8bbfd53338f21d8b2300603b070410e109130c5
maherkhatib77 <maher.khatib@gmail.com>
2025-07-06 18:29:51 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,3 @@
-
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
-import { getDatabase, ref, push, set, onValue, remove, update } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-database.js";
-
 const firebaseConfig = {
   apiKey: "AIzaSyCYEdHutsO1eYn3iQvnVehl-t96__Plwo4",
   authDomain: "educatalog-63603.firebaseapp.com",
@@ -11,8 +7,5 @@ const firebaseConfig = {
   messagingSenderId: "577201627806",
   appId: "1:577201627806:web:bd9d6346134d7c3dff518f"
 };
-
-const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
-
-export { db, ref, push, set, onValue, remove, update };
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
