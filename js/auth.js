0cea4933173e6f1c83b2a024dc38201bfb2cb36e
maherkhatib77 <maher.khatib@gmail.com>
2025-07-07 00:01:51 +0300

    Delete js directory

--- js/auth.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("adminLoginBtn").onclick = function () {
-  const user = prompt("שם משתמש:");
-  const pass = prompt("סיסמה:");
-  if (user === "Pisga" && pass === "Sara@2203") {
-    window.location.href = "dashboard.html";
-  } else {
-    alert("פרטי התחברות שגויים.");
-  }
-};
--- js/instructors.js
+++
@@ -1,1 +0,0 @@
-<!-- Placeholder for js/instructors.js -->
