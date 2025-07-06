48a28f409fa6f9f963c60f5650d5caea8979ae65
maherkhatib77 <maher.khatib@gmail.com>
2025-07-06 23:50:21 +0300

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+document.getElementById("loginButton").addEventListener("click", () => {
+  document.getElementById("loginModal").style.display = "block";
+});
+
+document.getElementById("loginSubmit").addEventListener("click", () => {
+  const user = document.getElementById("username").value;
+  const pass = document.getElementById("password").value;
+  if (user === "Pisga" && pass === "Sara@2203") {
+    window.location.href = "dashboard.html";
+  } else {
+    alert("שם משתמש או סיסמה שגויים.");
+  }
+});
