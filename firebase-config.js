a02116568a19df6497d74d8d7fa15217fd5800f3
maherkhatib77 <maher.khatib@gmail.com>
2025-07-11 08:24:24 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCgRQhK0u4xXRj4YOJhEtRm9nRxA_X8ALk",
+  authDomain: "educatalog-63603.firebaseapp.com",
+  databaseURL: "https://educatalog-63603-default-rtdb.firebaseio.com",
+  projectId: "educatalog-63603",
+  storageBucket: "educatalog-63603.appspot.com",
+  messagingSenderId: "577201627806",
+  appId: "1:577201627806:web:bd9d6346134d7c3dff518f"
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
