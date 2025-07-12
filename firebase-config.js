0156ee0e0977199079e9dab395c465d1e5ffdd97
maherkhatib77 <maher.khatib@gmail.com>
2025-07-12 08:51:58 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,5 @@
-<!-- Firebase App (core SDK) -->
-<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js"></script>
+// firebase-config.js
 
-<!-- Firebase Realtime Database -->
-<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js"></script>
-
-<!-- הגדרות הקונפיגורציה שלך -->
-<script>
 const firebaseConfig = {
   apiKey: "AIzaSyCgRQhK0u4xXRj4YOJhEtRm9nRxA_X8ALk",
   authDomain: "educatalog-63603.firebaseapp.com",
@@ -15,6 +9,6 @@ const firebaseConfig = {
   messagingSenderId: "577201627806",
   appId: "1:577201627806:web:bd9d6346134d7c3dff518f"
 };
-  // אתחול Firebase
-  firebase.initializeApp(firebaseConfig);
-</script>
+
+// אתחול Firebase
+firebase.initializeApp(firebaseConfig);
